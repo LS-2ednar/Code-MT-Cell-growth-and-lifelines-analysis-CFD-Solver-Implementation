a79a106eb224e7873fb62efb6892586a13b207f6
Lukas Schaub <73210826+LS-2ednar@users.noreply.github.com>
2026-02-06 09:42:42 +0100

    Update mpirun command to include --oversubscribe to run simpleFoam in parallel

--- 0_ExampleFolder/1_MAPPING/mapping1.sh
+++ 0_ExampleFolder/1_MAPPING/mapping1.sh
@@ -22,6 +22,6 @@ surfaceFeatures | tee surfaceFeatures.log
 snappyHexMesh -overwrite 2>&1  | tee snappyHexMesh.log
 checkMesh 2>&1 | tee checkMesh.log
 decomposePar 2>&1 | tee decomposPar.log
-mpirun -np 8 simpleFoam -parallel 2>&1 | tee simpleFoam.log
+mpirun --oversubscribe -np 8 simpleFoam -parallel 2>&1 | tee simpleFoam.log
 reconstructPar 2>&1 | tee reconsturctPar.log
 rm -r processor*
